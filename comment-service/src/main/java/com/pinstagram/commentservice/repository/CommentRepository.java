b3313d60a78c2b7a5d327cf7025cc117db7ee8eb
Marteldelfer <marteldelfer@gmail.com>
2025-06-20 19:10:41 -0300

    annotate as repository

--- comment-service/src/main/java/com/pinstagram/commentservice/repository/CommentRepository.java
+++ comment-service/src/main/java/com/pinstagram/commentservice/repository/CommentRepository.java
@@ -3,11 +3,13 @@ package com.pinstagram.commentservice.repository;
 import com.pinstagram.commentservice.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, UUID> {
 
     @Query("SELECT c FROM Comment c WHERE c.postId = :postId AND c.deleted = false AND c.parentId IS NULL")
