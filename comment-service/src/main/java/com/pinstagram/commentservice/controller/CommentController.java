ac9026ba9a8b43499b0adb0ba82977c7b2c68bd0
Marteldelfer <marteldelfer@gmail.com>
2025-06-20 02:58:10 -0300

    Add swagger documentation to comment service

--- comment-service/src/main/java/com/pinstagram/commentservice/controller/CommentController.java
+++ comment-service/src/main/java/com/pinstagram/commentservice/controller/CommentController.java
@@ -4,6 +4,8 @@ import com.pinstagram.commentservice.dto.CommentRequestDTO;
 import com.pinstagram.commentservice.dto.CommentResponseDTO;
 import com.pinstagram.commentservice.dto.CommentUpdateRequestDTO;
 import com.pinstagram.commentservice.service.CommentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/comments")
+@Tag(name = "Comments", description = "API for managing comments")
 public class CommentController {
 
     private final CommentService commentService;
@@ -22,31 +25,37 @@ public class CommentController {
     }
 
     @GetMapping
+    @Operation(description = "Find all comments")
     public ResponseEntity<List<CommentResponseDTO>> getAllComments() {
         return ResponseEntity.ok().body(commentService.getAllComments());
     }
 
     @GetMapping("/post/{postId}")
+    @Operation(description = "Find all comments with postId")
     public ResponseEntity<List<CommentResponseDTO>> findCommentsByPostId(@PathVariable("postId") UUID postId) {
         return ResponseEntity.ok().body(commentService.getAllCommentsByPostId(postId));
     }
 
     @GetMapping("/parent/{parentId}")
+    @Operation(description = "Find all children of comment with parentId")
     public ResponseEntity<List<CommentResponseDTO>> findCommentsByParentId(@PathVariable("parentId") UUID parentId) {
         return ResponseEntity.ok().body(commentService.getAllCommentsByParentId(parentId));
     }
 
     @GetMapping("/{commentId}")
+    @Operation(description = "Find Comment by commentId")
     public ResponseEntity<CommentResponseDTO> getCommentById(@PathVariable("commentId") UUID commentId) {
         return ResponseEntity.ok().body(commentService.getCommentById(commentId));
     }
 
     @PostMapping
+    @Operation(description = "Create new comment")
     public ResponseEntity<CommentResponseDTO> saveComment(@Valid @RequestBody CommentRequestDTO commentRequestDTO) {
         return ResponseEntity.ok().body(commentService.createComment(commentRequestDTO));
     }
 
     @PutMapping("/{commentId}")
+    @Operation(description = "Update comment content")
     public ResponseEntity<CommentResponseDTO> updateComment(
             @PathVariable("commentId") UUID commentId,
             @Valid@RequestBody CommentUpdateRequestDTO updateRequest
@@ -55,6 +64,7 @@ public class CommentController {
     }
 
     @DeleteMapping("/{commentId}")
+    @Operation(description = "Delete comment")
     public ResponseEntity<CommentResponseDTO> deleteCommentById(@PathVariable("commentId") UUID commentId) {
         commentService.deleteComment(commentId);
         return ResponseEntity.noContent().build();
