518c510b4b8558794d387eea09f7b956c6842ad1
Marteldelfer <marteldelfer@gmail.com>
2025-08-05 19:11:41 -0300

    Fix bugs in api-gateway

--- api-gateway/src/main/java/com/pinstagram/apigateway/filter/AddUserHeadersFilter.java
+++ api-gateway/src/main/java/com/pinstagram/apigateway/filter/AddUserHeadersFilter.java
@@ -5,6 +5,7 @@ import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -27,12 +28,24 @@ public class AddUserHeadersFilter implements GlobalFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        String token = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION); // token already validated
+        String token = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
+        if (token == null) {
+            ServerHttpRequest mutatedRequest = exchange.getRequest().mutate()
+                    .header("X-User-Name", "unknown")
+                    .header("X-User-Username", "unknown")
+                    .header("X-User-Id", "unknown")
+                    .header("X-User-Role", "unknown")
+                    .build();
+            ServerWebExchange mutatedExchange = exchange.mutate().request(mutatedRequest).build();
+            return chain.filter(mutatedExchange);
+        }
 
         return webClient.get()
                 .uri("/claims")
                 .header(HttpHeaders.AUTHORIZATION, token)
                 .retrieve()
+                .onStatus(HttpStatus.UNAUTHORIZED::equals, response -> Mono.empty())
                 .bodyToMono(new ParameterizedTypeReference<Map<String, String>>() {})
                 .flatMap(claims -> {
                     ServerHttpRequest mutatedRequest = exchange.getRequest().mutate()
